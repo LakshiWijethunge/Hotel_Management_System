7bf848b16859e3736fd6d0886e4ec62161638a32
Lakshi Wijethunge <lakshiwijethungesk@gmail.com>
2024-08-26 00:31:03 +0530

    initial project

---
+++ LuxeHotel/LuxeHotel/src/main/java/com/Luxedev/LuxeHotel/LuxeHotelApplication.java
@@ -0,0 +1,13 @@
+package com.Luxedev.LuxeHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxeHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxeHotelApplication.class, args);
+	}
+
+}
